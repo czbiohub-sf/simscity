e35c9739140af1e491e56962eba58bf7eaeb5c44
James Webber <james.webber@czbiohub.org>
2018-09-15 14:38:00 -0700

    added error-checking and broadcasting to sequencing functions

--- src/simscity/sequencing.py
+++ src/simscity/sequencing.py
@@ -30,22 +30,27 @@ def library_size(
     ).astype(int)
 
 
-def umi_counts(raw_expression: np.ndarray, lib_size: np.ndarray = None):
-    """Given an (n_obs, n_features) array of true expression values, generates
+def umi_counts(raw_expression: np.ndarray, lib_size: Union[float, np.ndarray] = None):
+    """Given an (n_obs, n_features) array of expression values, generates
     a count matrix of UMIs based by multinomial sampling with a variable-sized
     library of reads using a truncated log-normal distribution
 
-    :param raw_expression: expression of true expression values
-    :param lib_size: library size for each cell
-    :return: int array of shape (n_obs, n_features) containing umi counts
+    :param raw_expression: array of raw expression values (non-negative)
+    :param lib_size: library size for each cell, either constant or per-cell
+    :return: integer array of shape (n_obs, n_features) containing umi counts
     """
+
+    if np.any(raw_expression < 0):
+        raise ValueError("raw_expression must be non-negative")
 
     n_cells, n_genes = raw_expression.shape
 
     if lib_size is None:
         lib_size = library_size(n_cells)
+    else:
+        lib_size = np.broadcast_to(lib_size, (n_cells,))
 
-    gene_p = raw_expression / raw_expression.sum(1)[:, None]
+    gene_p = raw_expression / raw_expression.sum(1, keepdims=True)
 
     cell_gene_umis = np.vstack(
         [
@@ -58,25 +63,33 @@ def umi_counts(raw_expression: np.ndarray, lib_size: np.ndarray = None):
 
 
 def pcr_noise(
-    read_counts: np.ndarray, pcr_betas: np.ndarray, n: int, copy: bool = True
+    read_counts: np.ndarray,
+    pcr_betas: Union[float, np.ndarray],
+    n: int,
+    copy: bool = True,
 ):
     """PCR noise model: every read has an affinity for PCR, and for every round
     of PCR we do a ~binomial doubling of each count.
 
     :param read_counts: array of shape (n_cells, n_genes) representing unique
                         molecules
-    :param pcr_betas: read-specific PCR efficiencies of shape (n_genes,)
+    :param pcr_betas: PCR efficiency for each gene, either constant or per-gene
     :param n: number of rounds of PCR to simulate
     :param copy: return a copy of the read_counts array or modify in-place
     :return: int array of shape (n_cells, n_genes) with amplified counts
     """
+    if np.any(pcr_betas < 0):
+        raise ValueError("pcr_betas must be non-negative")
+
     if copy:
         read_counts = read_counts.copy()
+
+    pcr_betas = np.broadcast_to(pcr_betas, (1, read_counts.shape[1]))
 
     # for each round of pcr, each gene increases according to its affinity factor
     for i in range(n):
         read_counts += np.random.binomial(
-            read_counts, pcr_betas[None, :], size=read_counts.shape
+            n=read_counts, p=pcr_betas, size=read_counts.shape
         )
 
     return read_counts
