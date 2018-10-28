f50525dcb6be73a44c850a9c2e7975477b23c284
James Webber <james.webber@czbiohub.org>
2018-10-28 15:44:05 -0700

    Tweaked signature and docs to be consistent with other methods

--- src/simscity/drug.py
+++ src/simscity/drug.py
@@ -10,14 +10,17 @@ import simscity.latent
 
 
 def drug_projection(
-    n_latent: int, scale: Union[int, float], sparsity: float
+    n_latent: int, sparsity: float, scale: Union[int, float]
 ) -> np.ndarray:
     """Generates a linear weighting from a latent space to feature space,
     potentially with some coefficients set to zero. Returns the weighting.
 
     :param n_latent: dimensionality of the latent space
-    :param scale: scaling factor
-    :param sparsity: relative sparsity of the weighting vector
+    :param sparsity: probability that a program is important for the drug. An array
+                     of size ``(n_latent,)`` sets the rate per program
+    :param scale: scaling factor for program weighting. If an array
+                  of size ``(n_latent,)`` is given, different values are used
+                  for each of the programs
     :return: array of shape (n_latent,) with weighting
     """
 
@@ -33,16 +36,16 @@ def drug_doses(
 ) -> np.ndarray:
     """
     Generates an array of uniformly-spaced values with a bit of random noise
-    added in. Scaled to cover the expected range of the drug response data,
+    added in. Scaled to cover the expected range of the drug response data
 
     :param n_latent: dimensionality of the relevant latent space
-    :param scale: scaling factor used for z weights
+    :param scale: the scaling factor used for the drug projection
     :param n_conditions: number of conditions (doses) desired
     :return: array shape (n_conditions,) with thresholds
     """
 
     # expected scale of the dot product Xz
-    prod_scale = np.sqrt(n_latent * scale ** 2)
+    prod_scale = np.sqrt(n_latent) * scale
 
     dose_thresholds = np.linspace(
         -3 * prod_scale, 3 * prod_scale, n_conditions
